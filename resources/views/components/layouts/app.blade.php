3488bf67ad5ca53cb33d234b2d2f8916fed7824c
2509roblox <2509roblox@gmail.com>
2025-03-13 09:52:44 +0700

    donenene

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,10 +8,12 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="canonical" href="{{ url()->current() }}">
         <meta name="googlebot" content="index, follow">
+        <link rel="icon" href="{{ asset('icon/cropped-cropped-cropped-Thiet-ke-chua-co-ten-72x72.png') }}" type="image/x-icon">
         <meta name="author" content="Ly Ly Massage">
-        <meta name="description" content="Trang chủ - Ly Ly Massage">
-        <meta name="keywords" content="Trang chủ, Ly Ly Massage, Massage, Spa, Ly Ly, Massage tại nhà, Massage tại gia, Massage tại trọ, Massage tại khách sạn, Massage tại công ty, Massage tại trung tâm, Massage tại phòng khách, Massage tại phòng ngủ, Massage tại phòng ăn, Massage tại phòng học, Massage tại phòng làm việc, Massage tại phòng ngủ, Massage tại phòng ăn, Massage tại phòng học, Massage tại phòng làm việc">
+        <meta name="description" content="베트남 출장 마사지는 고객에게 집, 호텔 객실 또는 아파트에서 편안하고 편안한 경험을 제공합니다. 젊고 경험이 풍부한 기술자 팀이 다낭, 호이안, 나트랑, 달랏, 하노이, 호치민에서 연중무휴 24시간 비즈니스를 제공합니다. ">
+        <meta name="keywords" content="베트남 출장 마사지는 고객에게 집, 호텔 객실 또는 아파트에서 편안하고 편안한 경험을 제공합니다. 젊고 경험이 풍부한 기술자 팀이 다낭, 호이안, 나트랑, 달랏, 하노이, 호치민에서 연중무휴 24시간 비즈니스를 제공합니다. ">
         <meta name="google-site-verification" content="k7hhG1WTd9Q1nj3QJvhYQigcd23m1WYpOmpZmCAL8C0" />
+        <meta property="og:image" content="{{ asset('icon/banner.png') }}">
         @livewireStyles
     </head>
     <body>
