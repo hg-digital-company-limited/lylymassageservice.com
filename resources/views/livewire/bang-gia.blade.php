b9a6e7cf41fb3154fcb9e571eb8dce7b8ba41f59
2509roblox <2509roblox@gmail.com>
2025-03-13 08:48:10 +0700

    donenenne

--- resources/views/livewire/bang-gia.blade.php
+++ resources/views/livewire/bang-gia.blade.php
@@ -2819,7 +2819,7 @@
             id="swv-js"></script>
         <script type="text/javascript" id="contact-form-7-js-extra">
             /* <![CDATA[ */
-            var wpcf7 = { "api": { "root": "https:\/\/spa8.mauthemewp.com\/wp-json\/", "namespace": "contact-form-7\/v1" } };
+            var wpcf7 = { "api": { "root": "\/assets\/wp-json\/", "namespace": "contact-form-7\/v1" } };
             /* ]]> */
         </script>
         <script type="text/javascript"
@@ -2832,7 +2832,7 @@
             id="hoverIntent-js"></script>
         <script type="text/javascript" id="flatsome-js-js-extra">
             /* <![CDATA[ */
-            var flatsomeVars = { "theme": { "version": "3.18.3" }, "ajaxurl": "https:\/\/spa8.mauthemewp.com\/wp-admin\/admin-ajax.php", "rtl": "", "sticky_height": "83", "stickyHeaderHeight": "0", "scrollPaddingTop": "0", "assets_url": "https:\/\/spa8.mauthemewp.com\/wp-content\/themes\/flatsome\/assets\/", "lightbox": { "close_markup": "<button title=\"%title%\" type=\"button\" class=\"mfp-close\"><svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" width=\"28\" height=\"28\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-x\"><line x1=\"18\" y1=\"6\" x2=\"6\" y2=\"18\"><\/line><line x1=\"6\" y1=\"6\" x2=\"18\" y2=\"18\"><\/line><\/svg><\/button>", "close_btn_inside": false }, "user": { "can_edit_pages": false }, "i18n": { "mainMenu": "Menu ch\u00ednh", "toggleButton": "Chuy\u1ec3n \u0111\u1ed5i" }, "options": { "cookie_notice_version": "1", "swatches_layout": false, "swatches_disable_deselect": false, "swatches_box_select_event": false, "swatches_box_behavior_selected": false, "swatches_box_update_urls": "1", "swatches_box_reset": false, "swatches_box_reset_limited": false, "swatches_box_reset_extent": false, "swatches_box_reset_time": 300, "search_result_latency": "0" } };
+            var flatsomeVars = { "theme": { "version": "3.18.3" }, "ajaxurl": "\/assets\/wp-admin\/admin-ajax.php", "rtl": "", "sticky_height": "83", "stickyHeaderHeight": "0", "scrollPaddingTop": "0", "assets_url": "\/assets\/wp-content\/themes\/flatsome\/assets\/", "lightbox": { "close_markup": "<button title=\"%title%\" type=\"button\" class=\"mfp-close\"><svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" width=\"28\" height=\"28\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-x\"><line x1=\"18\" y1=\"6\" x2=\"6\" y2=\"18\"><\/line><line x1=\"6\" y1=\"6\" x2=\"18\" y2=\"18\"><\/line><\/svg><\/button>", "close_btn_inside": false }, "user": { "can_edit_pages": false }, "i18n": { "mainMenu": "Menu ch\u00ednh", "toggleButton": "Chuy\u1ec3n \u0111\u1ed5i" }, "options": { "cookie_notice_version": "1", "swatches_layout": false, "swatches_disable_deselect": false, "swatches_box_select_event": false, "swatches_box_behavior_selected": false, "swatches_box_update_urls": "1", "swatches_box_reset": false, "swatches_box_reset_limited": false, "swatches_box_reset_extent": false, "swatches_box_reset_time": 300, "search_result_latency": "0" } };
             /* ]]> */
         </script>
         <script type="text/javascript"
